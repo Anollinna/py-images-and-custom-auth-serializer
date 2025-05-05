c3bb8f3f69170270d9f18a353429616044d3fb7b
Khrystyna Sen <anollina@ukr.net>
2025-05-05 22:58:39 +0300

    Update 0003_alter_movie_image.py

--- cinema/migrations/0003_alter_movie_image.py
+++ cinema/migrations/0003_alter_movie_image.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             model_name="movie",
             name="image",
             field=models.ImageField(
-                null=True, upload_to=cinema.models.movie_image_path
+                null=True, upload_to="upload/movies/"
             ),
         ),
     ]
