7319d678d585aefedc97b8d879dfce4d34bfedb4
Khrystyna Sen <anollina@ukr.net>
2025-05-05 19:42:15 +0300

    Update serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -34,12 +34,12 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = (
-            "id", 
-            "title", 
-            "description", 
-            "duration", 
-            "genres", 
-            "actors", 
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
             "image"
         )
 
